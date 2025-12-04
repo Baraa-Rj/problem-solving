814cf69c7092574fea930a0fe1e3ecee9c707bc0
Baraa RJ <baraarjoob17@gmail.com>
2025-12-04 18:50:03 +0200

    Add isPalindromeDigit.java - palindrome number checker

--- src/isPalindromeDigit.java
+++ src/isPalindromeDigit.java
@@ -1,2 +1,20 @@
-public class isPalindromeDigit {
+class isPalindromeDigit {
+    public boolean isPalindrome(int x) {
+        String value = String.valueOf(x);
+        int left = 0;
+        int right = value.length() - 1;
+        while (left < right) {
+            if (value.charAt(left) != value.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isPalindromeDigit isPalindromeDigit = new isPalindromeDigit();
+        System.out.println(isPalindromeDigit.isPalindrome(121));
+    }
 }
