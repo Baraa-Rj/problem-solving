ab4a7d69fb65f5094f58e1845ee9782d0a2cb778
Baraa RJ <baraarjoob17@gmail.com>
2025-12-05 20:57:31 +0200

    Second solution

--- src/CountPartitionsWithEvenSumDifference.java
+++ src/CountPartitionsWithEvenSumDifference.java
@@ -7,6 +7,26 @@ class CountPartitionsWithEvenSumDifference {
         }
         return totalSum %2 == 0 ? nums.length-1 : 0;
     }
+    class Solution {
+        public int countPartitions(int[] nums) {
+            int total = 0;
+            for (int num : nums) total += num;
+
+            int leftSum = 0;
+            int count = 0;
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                leftSum += nums[i];
+                int rightSum = total - leftSum;
+
+                if ((leftSum % 2) == (rightSum % 2)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
 
     public static void main(String[] args) {
         CountPartitionsWithEvenSumDifference countPartitionsWithEvenSumDifference = new CountPartitionsWithEvenSumDifference();
