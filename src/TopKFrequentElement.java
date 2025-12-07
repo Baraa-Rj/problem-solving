29a18655c3b1b4c5e695ecb71363e0d230fdf37b
Baraa RJ <baraarjoob17@gmail.com>
2025-12-07 13:46:07 +0200

    Bucket Sort

--- src/TopKFrequentElement.java
+++ src/TopKFrequentElement.java
@@ -1,10 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class TopKFrequentElement {
 
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] BucketSort(int[] nums, int k) {
        Map<Integer, Integer> map = new HashMap<>();
        for (int num : nums) {
            map.put(num, map.getOrDefault(num, 0) + 1);
@@ -16,6 +14,33 @@ public class TopKFrequentElement {
            result[i] = queue.poll().getKey();
        }
        return result;
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
     }
 
 
