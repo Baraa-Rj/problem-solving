6c6bc886e353e2b24df06877b3cb01500f33e6b2
Baraa RJ <baraarjoob17@gmail.com>
2025-12-07 13:01:45 +0200

    count odds with O(1)

--- src/CountOdds.java
+++ src/CountOdds.java
@@ -10,6 +10,13 @@ public class CountOdds {
         }
         return count;
     }
+    public int countOddsOOfOne (int low, int high) {
+        int range = high - low +1;
+        if(high%2 != 0 && low %2 != 0){
+            return range/2 +1;
+        }
+        return range /2;
+    }
     public static void main(String[] args) {
         CountOdds countOdds = new CountOdds();
         System.out.println(countOdds.countOdds(1,10));
