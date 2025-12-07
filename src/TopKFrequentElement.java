c2c1b64bb7644c1ab5edced200c0b69a5b424912
Baraa RJ <baraarjoob17@gmail.com>
2025-12-07 13:41:57 +0200

    Top K Frequent Element

--- src/TopKFrequentElement.java
+++ src/TopKFrequentElement.java
@@ -1,3 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class TopKFrequentElement {
 
+    public int[] topKFrequent(int[] nums, int k) {
+       Map<Integer, Integer> map = new HashMap<>();
+       for (int num : nums) {
+           map.put(num, map.getOrDefault(num, 0) + 1);
+       }
+       PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+       queue.addAll(map.entrySet());
+       int[] result = new int[k];
+       for (int i = 0; i < k; i++) {
+           result[i] = queue.poll().getKey();
+       }
+       return result;
+    }
+
+
+    public static void main(String[] args) {
+        TopKFrequentElement topKFrequentElement = new TopKFrequentElement();
+        int[] nums = {1,2,2,3,3,3};
+        int k = 2;
+        int[] result = topKFrequentElement.topKFrequent(nums, k);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 }
