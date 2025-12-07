8b1022ca0b02e8d060573f6d6aef876473822775
Baraa RJ <baraarjoob17@gmail.com>
2025-12-07 13:47:00 +0200

    Baraa

--- src/TopKFrequentElement.java
+++ src/TopKFrequentElement.java
@@ -43,6 +43,25 @@ public class TopKFrequentElement {
         return res;
     }
 
+        public int[] Sorting(int[] nums, int k) {
+            Map<Integer, Integer> count = new HashMap<>();
+            for (int num : nums) {
+                count.put(num, count.getOrDefault(num, 0) + 1);
+            }
+
+            List<int[]> arr = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+                arr.add(new int[] {entry.getValue(), entry.getKey()});
+            }
+            arr.sort((a, b) -> b[0] - a[0]);
+
+            int[] res = new int[k];
+            for (int i = 0; i < k; i++) {
+                res[i] = arr.get(i)[1];
+            }
+            return res;
+        }
+
 
     public static void main(String[] args) {
         TopKFrequentElement topKFrequentElement = new TopKFrequentElement();
